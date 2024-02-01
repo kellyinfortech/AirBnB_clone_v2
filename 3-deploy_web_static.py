d056bf4ef32aad3df00ca06cbb5033220a6771ad
kellyinfortech <kellyinfortech.solution@gmail.com>
2024-02-01 12:12:58 -0800

    deploy static web full

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['44.192.38.3', '3.239.82.120']
+env.hosts = ['54.152.76.26', '34.232.53.218']
 
 
 def do_pack():
